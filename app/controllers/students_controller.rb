f71e0bc30a2555ec71bee1bc6f4d93a48ed0b80b
Sun-Mountain <nzonnenberg@fearless.tech>
2021-05-10 20:15:19 -0400

    Student update

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new student_parms
+    @student = Student.new student_params
 
     if @student.save
       flash[:notice] = 'Student added.'
@@ -22,10 +22,30 @@ class StudentsController < ApplicationController
     redirect_to students_path
   end
 
+  def show
+    @user = user
+    @team = team
+    @student = Student.find(student_id)
+  end
+
   def edit
+    @user = user
+    @team = team
+    @student = Student.find(student_id)
   end
 
   def update
+    @user = user
+    @team = team
+    @student = Student.find(student_id)
+
+    if @student.update(student_params)
+      flash[:notice] = 'Team updated.'
+      redirect_to user_team_student_path(id: @student.id, user_id: @user.id, team_id: @team.id)
+    else
+      flash[:alert] = "Student could not be updated: #{model_error_string(@student)}"
+      render :edit
+    end
   end
 
   def destroy
@@ -50,7 +70,7 @@ class StudentsController < ApplicationController
     params[:id]
   end
 
-  def student_parms
+  def student_params
     params.require(:student).permit(:id, :first_name, :last_name, :email)
   end
 
