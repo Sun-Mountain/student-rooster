009ba1dd20fed7b789506c52c34a4f8eedc2d031
Sun-Mountain <nzonnenberg@fearless.tech>
2021-05-11 16:57:30 -0400

    Student can be deleted

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -50,6 +50,7 @@ class StudentsController < ApplicationController
 
   def destroy
     @student = Student.find(student_id)
+    destroy_unique_rs(@student.id)
     
     if @student.destroy
       flash[:notice] = 'Student deleted.'
@@ -79,6 +80,13 @@ class StudentsController < ApplicationController
 
   def delete_unique_roster(unique_roster_id)
     UniqueRoster.find(unique_roster_id).destroy
+  end
+
+  def destroy_unique_rs(student_id)
+    u_r_ls = UniqueRosterLink.where(student_id: student_id)
+    u_rs = UniqueRoster.where(student_id: student_id)
+    u_r_ls.destroy_all
+    u_rs.destroy_all
   end
 
   def user
