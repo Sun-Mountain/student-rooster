8237d20e3c63a51740efdf037cbc40f6cad6bd3f
Sun-Mountain <nzonnenberg@fearless.tech>
2021-05-12 15:13:07 -0400

    User creates account test

--- spec/features/registration/new_user_signs_up_for_account_spec.rb
+++ spec/features/registration/new_user_signs_up_for_account_spec.rb
@@ -2,9 +2,35 @@
 
 require 'rails_helper'
 
-RSpec.feature 'new user signs up for account' do
+RSpec.feature 'user creates account' do
   before(:each) do
     visit new_user_session_path
     click_link 'sign-up'
+  end
+
+  context 'success:' do
+    scenario 'user creates an account' do
+      fill_in :user_username, with: 'CapPicard'
+      fill_in :user_full_name, with: 'Jean Luc Picard'
+      fill_in :user_email, with: 'picard@gmail.com'
+      fill_in :user_password, with: 'password'
+      fill_in :user_password_confirmation, with: 'password'
+
+      click_on('Sign up')
+
+      expect(page).to have_content('Welcome! You have signed up successfully.')
+      expect(page).to have_content('Create Your Team')
+    end
+  end
+
+  context 'fail:' do
+    scenario 'when missing required fields' do
+      click_on('Sign up')
+
+      expect(page).to have_content("Email can't be blank")
+      expect(page).to have_content("Password can't be blank")
+      expect(page).to have_content("Username can't be blank")
+      expect(page).to have_content("Full name can't be blank")
+    end
   end
 end
