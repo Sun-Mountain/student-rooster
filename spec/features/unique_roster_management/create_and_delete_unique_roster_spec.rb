c3011a402e6eecd7a37e942950f13a30a18047eb
Sun-Mountain <nzonnenberg@fearless.tech>
2021-08-28 16:07:28 -0400

    Create and delete unique roster test

---
+++ spec/features/unique_roster_management/create_and_delete_unique_roster_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+RSpec.feature 'creating and deleting unique rosters' do
+  let!(:user) { create :user }
+  let!(:team) { create :team }
+  let!(:lesson) { create :lesson }
+  let!(:student) { create :student }
+  let!(:roster) { create :roster }
+
+  
+  describe 'successful' do
+    before(:each) do
+      @team = create(:team)
+      @lesson = create(:lesson)
+      @student = create(:student)
+      @roster = create(:roster)
+
+      user.teams << @team
+      @team.lessons << @lesson
+      @lesson.rosters << @roster
+      @team.students << @student
+      sign_in user
+    end
+
+    context 'student page' do
+      scenario 'create and delete single unique roster' do
+        visit team_student_path(@team.id, @student.id)
+
+        expect(page.body).to have_content("This student has not been enrolled in any classes.")
+        expect(@student.unique_rosters).to be_empty
+
+        find('#unique_roster_lesson_id').find(:xpath, 'option[2]').select_option
+        find('#unique_roster_roster_id').find(:xpath, 'option[2]').select_option
+        find('[data-test="add-unique-roster"]').click
+
+        expect(page.body).to have_content("Student added to Roster!")
+        expect(@student.unique_rosters).not_to be_empty
+
+        @unique_roster = @student.unique_rosters.first
+
+        find("[data-test=\"delete-ur-#{@unique_roster.id}\"]").click
+
+        expect(@student.unique_rosters).to be_empty
+      end
+    end
+  end
+end
