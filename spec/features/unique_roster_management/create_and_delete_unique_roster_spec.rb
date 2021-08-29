f278e1864d47d51a6b4566af9861f48b426c4b0b
Sun-Mountain <nzonnenberg@fearless.tech>
2021-08-28 20:42:02 -0400

    Add unique roster uniqueness test

--- spec/features/unique_roster_management/create_and_delete_unique_roster_spec.rb
+++ spec/features/unique_roster_management/create_and_delete_unique_roster_spec.rb
@@ -79,5 +79,24 @@ RSpec.feature 'creating and deleting unique rosters' do
         expect(@student.unique_rosters).to be_empty
       end
     end
+
+    scenario 'unique roster must be unique' do
+      visit team_student_path(@team.id, @student.id)
+
+      expect(@student.unique_rosters.count).to eq(0)
+
+      find('#unique_roster_lesson_id').find(:xpath, 'option[2]').select_option
+      find('#unique_roster_roster_id').find(:xpath, 'option[2]').select_option
+      find('[data-test="add-unique-roster"]').click
+
+      expect(@student.unique_rosters.count).to eq(1)
+
+      find('#unique_roster_lesson_id').find(:xpath, 'option[2]').select_option
+      find('#unique_roster_roster_id').find(:xpath, 'option[2]').select_option
+      find('[data-test="add-unique-roster"]').click
+
+      expect(page.body).to have_content("Student could not be added: Student already enrolled.")
+      expect(@student.unique_rosters.count).to eq(1)
+    end
   end
 end
