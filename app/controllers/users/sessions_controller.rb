e89ba4a84569373595946d46ed94cce3225ecd99
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-12-06 11:38:54 -0500

    user controllers

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  private
+
+  def respond_with(_resource, _opts = {})
+    render json: {
+      message: 'Logged in successfully.',
+      user: current_user
+    }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    log_out_success && return if current_user
+    log_out_failure
+  end
+
+  def log_out_success
+    render json: { message: 'Logged out successfully.' }, status: :ok
+  end
+
+  def log_out_failure
+    render json: { message: 'Log out failed.' }, status: :unauthorized
+  end
+end
