50e5d15f95f93e6815a96e17a3db4d9790628e6c
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-12-06 11:32:32 -0500

    members controller

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,21 @@
+class MembersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    user = get_user_from_token
+    render json: {
+      message: "You're in!",
+      user: user
+    }
+    # TODO: Add error handling
+  end
+
+  private
+
+  def get_user_from_token
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
+                      Rails.application.credentials.devise[:jwt_secret_key]).first
+    user_id = jwt_payload['sub']
+    user = User.find(user_id.to_s)
+  end
+end
