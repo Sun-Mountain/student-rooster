456229260ae1b176acae055dd42df18f8b226855
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-01-05 11:22:21 -0500

    Setup backend for devise api

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,14 @@ module StudentRooster
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # This also configures session_optiosn for use below
+    config.session_store :cookie_store, key: '_interslice_session'
+
+    # Required for all session management (regardless of session_store)
+    config.middleware.use ActionDispatch::Cookies
+
+    config.middleware.use config.session_store, config.session_options
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             controllers: {
+              sessions: 'users/sessions',
+              registrations: 'users/registrations'
+             }
+  get '/member-data', to: 'members#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
