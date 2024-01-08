4df10a6637fd9bbd5929ebb460d7f89f20e6c80c
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-01-08 14:59:36 -0500

    basic spacing

--- app/components/color_swatch_component.rb
+++ app/components/color_swatch_component.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 class ColorSwatchComponent < ViewComponent::Base
-  def initialize(colorCode:, colorCode50:, colorCode30:, title:)
+  def initialize(colorCode:,
+                 colorCode50:,
+                 colorCode30:,
+                 textColor: 'rgba(73, 80, 87, 1)',
+                 lightTextColor: 'rgba(73, 80, 87, 1)',
+                 title: 'No Title')
     @colorCode = colorCode
     @colorCode50 = colorCode50
     @colorCode30 = colorCode30
+    @lightTextColor = lightTextColor
     @title = title
   end
 end
