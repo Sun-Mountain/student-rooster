21bbfc33fb041aa55f6cdac36ba7a62435b25262
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-01-19 10:34:45 -0500

    update db

--- db/migrate/20240109233415_create_memberships.rb
+++ db/migrate/20240109233415_create_memberships.rb
@@ -1,8 +1,11 @@
 class CreateMemberships < ActiveRecord::Migration[7.1]
   def change
     create_table :memberships do |t|
-      t.references :team, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :team,     null: false,  foreign_key: true
+      t.references :user,     null: true,   foreign_key: true
+      t.string :email,        null: false,  default: ''
+      t.boolean :accepted,    null: false,  default: false
+      t.boolean :no_account,  null: false,  default: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_19_144357) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_16_183612) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,11 +24,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_19_144357) do
 
   create_table "memberships", force: :cascade do |t|
     t.bigint "team_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.string "email", default: "", null: false
     t.boolean "accepted", default: false, null: false
+    t.boolean "no_account", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["team_id"], name: "index_memberships_on_team_id"
     t.index ["user_id"], name: "index_memberships_on_user_id"
   end
