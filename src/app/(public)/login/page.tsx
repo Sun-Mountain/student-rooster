fd3606f3ba1dbdabad464032339c1922b15b1b3f
Nicole Zonnenberg <nicolezonnenberg@Nicoles-MacBook-Pro.local>
2025-09-09 14:58:11 -0400

    login form styling

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,7 +1,27 @@
-import UnderConstruction from "@/components/_Content/UnderConstruction";
+import { Form } from "@base-ui-components/react/form";
+import TextField from "@/components/_UI/Forms/TextField";
+import Button from "@/components/_UI/Button";
 
 const LoginPage = () => {
-  return <UnderConstruction />;
+  return (
+    <>
+      <div className="form-container">
+        <h1>Login</h1>
+        <Form>
+          <TextField label="Email" name="email" type="email" required />
+          <TextField label="Password" name="password" type="password" required />
+          <div className="btn-container">
+            <Button type="submit" className="submit-button">
+              Login
+            </Button>
+          </div>
+        </Form>
+      </div>
+      <div className="center-content">
+        <p>Don't have an account? <a href="/sign-up">Sign Up</a></p>
+      </div>
+    </>
+  );
 }
 
 export default LoginPage;
