c31715e428a38269fa623b03d6be8713f2bd0e0c
Nicole Zonnenberg <nicolezonnenberg@Nicoles-MacBook-Pro.local>
2025-09-16 14:12:42 -0400

    install mui

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -4,7 +4,11 @@ import { SessionProvider as NextSessionProvider } from "next-auth/react";
 import { FC, ReactNode } from "react";
 
 const Provider: FC<{ children: ReactNode }> = ({ children }) => {
-  return <NextSessionProvider>{children}</NextSessionProvider>;
+  return (
+    <NextSessionProvider>
+      {children}
+    </NextSessionProvider>
+  );
 };
 
 export default Provider;
