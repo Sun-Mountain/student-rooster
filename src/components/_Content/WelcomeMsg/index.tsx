4e31a71e245af2572dfd9e7b1a5f2a386eb436a8
Nicole Zonnenberg <nicolezonnenberg@Nicoles-MacBook-Pro.local>
2025-09-16 13:28:30 -0400

    update welcome message

--- src/components/_Content/WelcomeMsg/index.tsx
+++ src/components/_Content/WelcomeMsg/index.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 const WelcomeMsg = () => {
   return (
     <div className="content-container welcome-msg">
       <h1>Welcome to Student Rooster!</h1>
-      <p>Thank you for checking out our app! We are still very new and very under construction so please bear with us. In the meantime, feel free to explore the login and sign-up pages (though they are not functional yet).</p>
+      <p>Thank you for checking out our app! We are still very new and very under construction so please bear with us. In the meantime, feel free to explore the <Link href="/login">login</Link> and <Link href="/sign-up">sign-up</Link> pages.</p>
       <p>We appreciate your patience and support as we continue to build and improve Student Rooster.</p>
       <p>Stay tuned for more updates and features coming soon!</p>
       <p>Best regards,</p>
